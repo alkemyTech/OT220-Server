f065534cefbe1b4f6b6757b0ae0d10149d96657b
Danielid Ramirez <danielid132@gmail.com>
2022-05-19 20:36:38 -0400

    feat OT220-16 add: model migration and controller roles

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 2022_05_20_205115) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
