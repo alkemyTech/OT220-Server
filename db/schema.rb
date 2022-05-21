e502378c826e6837fa687ee0d09b9791e775d225
Ender Ruiz <ruiz.valera@gmail.com>
2022-05-21 16:22:22 -0400

    add model migration and controller testimonials OT-220-20

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_20_205115) do
+ActiveRecord::Schema.define(version: 2022_05_21_183248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,15 @@ ActiveRecord::Schema.define(version: 2022_05_20_205115) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "testimonials", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "content"
+    t.datetime "discarded_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["discarded_at"], name: "index_testimonials_on_discarded_at"
   end
 
   create_table "users", force: :cascade do |t|
