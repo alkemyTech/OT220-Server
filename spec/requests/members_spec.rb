f35cf7c15d8cc9bee51833b6ab39ff02dd270246
Angela Cuadros <angelascc16@gmail.com>
2022-05-25 13:45:58 -0500

    feat OT220-21: add model migration and controller for member

--- spec/requests/members_spec.rb
+++ spec/requests/members_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe 'Members', type: :request do
   describe 'GET /index' do
     pending "add some examples (or delete) #{__FILE__}"
   end
