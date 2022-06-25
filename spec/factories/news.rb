24d8eee8248809b8083fd22d4405cc58259ee0d4
Joaquin Logiudice <juakoloyu@gmail.com>
2022-06-24 22:12:36 -0300

    feat OT-220-98: testing news

--- spec/factories/news.rb
+++ spec/factories/news.rb
@@ -5,5 +5,21 @@ FactoryBot.define do
     name { 'MyString' }
     content { 'MyText' }
     image { 'MyString' }
+    category { create(:category) }
+
+    trait :good do
+      name { Faker::Lorem.sentence }
+      content { Faker::Lorem.paragraph }
+      image { Faker::Lorem.sentence }
+    end
+
+    trait :bad do
+      name { nil }
+      content { nil }
+      image { nil }
+    end
+
+    factory :good_news, traits: [:good]
+    factory :bad_news, traits: [:bad]
   end
 end
